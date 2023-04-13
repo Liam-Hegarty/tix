e6dd1ade313204af22f33b8e28e077756dd3e806
L-Heggerz <liam.hegarty@btinternet.com>
2023-04-13 18:43:16 +0100

    Add first splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { SplashScreens } from "./SplashScreens";
 
 function App() {
-  return <>Tix</>;
+  const [splashScreen, setSplashScreen] = useState<string | undefined>("cool");
+
+  if (splashScreen) {
+    return <SplashScreens {...{ splashScreen, setSplashScreen }} />;
+  } else {
+    return <>Menu</>;
+  }
 }
 
 export default App;
---
+++ src/SplashScreens.tsx
@@ -0,0 +1,63 @@
+import React, { Dispatch, SetStateAction, useEffect } from "react";
+import { Box } from "@mui/material";
+
+export const SplashScreens = ({
+  splashScreen,
+  setSplashScreen,
+}: {
+  splashScreen: string;
+  setSplashScreen: Dispatch<SetStateAction<string | undefined>>;
+}) => {
+  useEffect(() => {
+    const tix = setTimeout(() => setSplashScreen("tix"), 3000);
+    const end = setTimeout(() => setSplashScreen(undefined), 6000);
+    return () => {
+      clearTimeout(tix);
+      clearTimeout(end);
+    };
+  });
+
+  switch (splashScreen) {
+    case "cool":
+      return (
+        <Box
+          className="fade-in"
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            width: "100%",
+            height: "100vh",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            src={`${process.env.PUBLIC_URL}/cool.png`}
+            alt={`/cool.png`}
+            width={1496 / 4}
+            height={1584 / 4}
+          />
+          <p style={{ color: "white" }}>HEG.COOL</p>
+        </Box>
+      );
+    case "tix":
+      return (
+        <Box
+          className="fade-in"
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            width: "100%",
+            height: "100vh",
+            flexDirection: "column",
+            fontSize: "50"
+          }}
+        >
+          TIX
+        </Box>
+      );
+    default:
+      return <></>;
+  }
+};
