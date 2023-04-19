2d0edce4293b4e9d08fd4da73a709496a8de4e08
Liam Hegarty <liam.hegarty@beamery.com>
2023-04-19 23:48:04 +0100

    programmable faces!

--- src/game/robot/CrashedRobot.tsx
+++ src/game/robot/CrashedRobot.tsx
@@ -1,5 +1,7 @@
 import { Container, Sprite, useTick } from "@pixi/react";
 import React, { useState } from "react";
+import { SadFace } from "./faces";
+import colors from "../../palette";
 
 export const CrashedRobot = ({ spacing }: { spacing: number }) => {
   const [rotation, setRotation] = useState(0);
@@ -15,6 +17,12 @@ export const CrashedRobot = ({ spacing }: { spacing: number }) => {
         anchor={{ x: 0.5, y: 1 }}
         scale={{ x: 0.05, y: 0.05 }}
       />
+      <Container
+        x={-6}
+        y={-55}
+      >
+        <SadFace color={colors.darkBlue} />
+      </Container>
       <Sprite
         image={`${process.env.PUBLIC_URL}/sprite/mini/exclamation.png`}
         x={spacing / 16}
--- src/game/robot/HappyRobot.tsx
+++ src/game/robot/HappyRobot.tsx
@@ -1,12 +1,22 @@
-import { Sprite } from "@pixi/react";
+import { Container, Sprite } from "@pixi/react";
 import React from "react";
+import { HappyFace } from "./faces";
+import colors from "../../palette"
 
 export const HappyRobot = () => {
   return (
-    <Sprite
-      image={`${process.env.PUBLIC_URL}/sprite/robot.png`}
-      anchor={{ x: 0.5, y: 1 }}
-      scale={{ x: 0.05, y: 0.05 }}
-    />
+    <>
+      <Sprite
+        image={`${process.env.PUBLIC_URL}/sprite/robot.png`}
+        anchor={{ x: 0.5, y: 1 }}
+        scale={{ x: 0.05, y: 0.05 }}
+      />
+      <Container
+        x={-6}
+        y={-55}
+      >
+        <HappyFace color={colors.darkBlue} />
+      </Container>
+    </>
   );
 };
