da5d7ab5a790e4b41ba18af8a78af2147eb57b33
Liam Hegarty <liam.hegarty@beamery.com>
2023-04-20 16:47:36 +0100

    make it so you can't move on tocks

--- src/game/events/addEventListeners.ts
+++ src/game/events/addEventListeners.ts
@@ -36,7 +36,8 @@ const moveIsOnTempo =
       dividend - previousBeat.time,
       nextBeat.time - dividend
     );
-    if (distanceFromBeat < level.music.tolerance) {
+
+    if (distanceFromBeat < level.music.tolerance && (nextBeat.tock === e.action)) {
       return undefined;
     } else {
       return {
