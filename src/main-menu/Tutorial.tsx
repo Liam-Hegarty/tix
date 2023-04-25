d5cdc09655619521285a58d20de26849befbf2b4
Liam Hegarty <liam.hegarty@beamery.com>
2023-04-25 17:31:11 +0100

    fix tutorial style

--- src/main-menu/Tutorial.tsx
+++ src/main-menu/Tutorial.tsx
@@ -1,30 +1,22 @@
 import { Box } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import constants from "../constants";
+import { mui } from "../palette";
 
 const tutorialRead = () => {
   localStorage.setItem(constants.tutorialKey, "true");
 };
 
 export const Tutorial = ({ nextStage }: { nextStage: () => void }) => {
-  const [{ width, height }, setDimension] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
   useEffect(() => {
-    const resize = () =>
-      setDimension({ width: window.innerWidth, height: window.innerHeight });
     const markAsRead = setTimeout(tutorialRead, 3000);
 
     window.addEventListener("keydown", nextStage);
     window.addEventListener("mousedown", nextStage);
-    window.addEventListener("resize", resize);
 
     return () => {
       window.removeEventListener("keydown", nextStage);
       document.removeEventListener("mousedown", nextStage);
-      window.removeEventListener("resize", resize);
       clearTimeout(markAsRead);
     };
   }, [nextStage]);
@@ -38,16 +30,19 @@ export const Tutorial = ({ nextStage }: { nextStage: () => void }) => {
         width: "100%",
         height: "100vh",
         flexDirection: "column",
-        backgroundColor: "#111111",
+        backgroundColor: mui.darkBlue,
         color: "white",
       }}
     >
       <img
         src={`${process.env.PUBLIC_URL}/screens/tutorial.png`}
         alt={`/tutorial.png`}
-        width={width}
-        height={height}
-        object-fit={"cover"}
+        style={{
+          maxWidth: "95vw",
+          maxHeight: "95vh",
+          height: "auto",
+          border: "3px solid #FFFFFF"
+        }}
       />
     </Box>
   );
