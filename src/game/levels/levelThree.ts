6fe3f188a90bd506d58ec63276e56bcd9ccfda1a
Liam Hegarty <liam.hegarty@beamery.com>
2023-04-25 21:36:29 +0100

    finish level 3

--- src/game/levels/levelThree.ts
+++ src/game/levels/levelThree.ts
@@ -2,30 +2,85 @@ import { Level } from "./LevelTypes";
 import { parseGrid } from "./gridParser";
 
 const levelThreeRaw = `
-  +++        .
-  +O++X      .
-  +++        .
-             .
+  O++   .
+    D   .
+    +++ .
+      D .
+    +++ .
+     D  .
+    +++ .
+      D .
+    +++ .
+    D   .
+    +++ .
+      D .
+    +++ .
+     D   .
+  X+++  .
 `;
 
 export const levelThree: Level = {
   ...parseGrid(levelThreeRaw),
   music: {
     rhythm: [
-      { tock: true, time: 200 },
-      { tock: false, time: 200 },
-      { tock: false, time: 200 },
-      { tock: true, time: 200 },
-      { tock: false, time: 200 },
-      { tock: false, time: 200 },
       { tock: true, time: 400 },
+      { tock: false, time: 400 },
+      { tock: false, time: 400 },
       { tock: false, time: 400 },
     ],
     rhythmOffset: 25,
-    audioPath: "audio/music/level3.mp3",
-    tolerance: 100,
+    audioPath: "audio/music/future-chase-level.mp3",
+    tolerance: 160,
   },
-  scannerDrones: [],
+  scannerDrones: [
+    {
+      location: { x: 5, y: 1 },
+      area: {
+        topLeft: { x: 4, y: 2 },
+        width: 3,
+        height: 1,
+      },
+    },
+    {
+      location: { x: 5, y: 3 },
+      area: {
+        topLeft: { x: 4, y: 4 },
+        width: 3,
+        height: 1,
+      },
+    },
+    {
+      location: { x: 5, y: 5 },
+      area: {
+        topLeft: { x: 4, y: 6 },
+        width: 3,
+        height: 1,
+      },
+    },
+    {
+      location: { x: 5, y: 7 },
+      area: {
+        topLeft: { x: 4, y: 8 },
+        width: 3,
+        height: 1,
+      },
+    },
+    {
+      location: { x: 5, y: 9 },
+      area: {
+        topLeft: { x: 4, y: 10 },
+        width: 3,
+        height: 1,
+      },
+    },
+    {
+      location: { x: 5, y: 11 },
+      area: {
+        topLeft: { x: 4, y: 12 },
+        width: 3,
+        height: 1,
+      },
+    },
+  ],
   zapTiles: [],
-  doors: [],
 };
